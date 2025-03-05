964ce15c957b4018ad42b4865a959711350cb5f8
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-03-05 18:56:18 +0700

    base set up

---
+++ components/Fotter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fotter = () => {
+  return <div className="h-[22.5rem] w-full bg-amber-800">Fotter</div>;
+};
+
+export default Fotter;
---
+++ components/TopNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNav = () => {
+  return <div className="h-[5.5rem] bg-amber-100 w-full">TopNav</div>;
+};
+
+export default TopNav;
