44cba649a3a7b81d06e193ac4a2785f3cfd89ac9
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-03-06 16:41:19 +0700

    add nav and pages

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen text-4xl">
+    <div className="flex flex-col items-center justify-center min-h-screen text-4xl">
       <h1>A+N Architects</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
---
+++ app/team/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-4xl">
+      <h1>Team Page</h1>
+    </div>
+  );
+};
+
+export default page;
