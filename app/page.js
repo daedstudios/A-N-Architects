603227aee16b6c21293b903a90f8e01c77e00dcd
eliasheininger <eliasheininger@web.de>
2025-03-07 22:53:15 +0700

    bug margin top 2

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     
         <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-auto grid-rows-[repeat(13,_22.5rem)] lg:grid-rows-[repeat(8,_22.5rem)] px-2 gap-2 min-h-screen text-4xl">
-           <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1 place-content-end"> ALT </div>
+           <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1 mt-auto"> ALT </div>
        
            <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1   "> +NEU </div>
      
