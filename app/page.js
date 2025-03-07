b10d9b50004e98943b2bbf4f146781ecea13f58a
Pawina85 <pawinachanthachon@gmail.com>
2025-03-07 20:09:55 +0700

    adding contact section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
+import Contact from "@/components/ContactUsBanner/Contact";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-auto grid-rows-[repeat(11,_22.5rem)]  px-2 gap-2 min-h-screen text-4xl">
-      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1 place-content-end"> ALT </div>
+      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1 place-content-end">
+        {" "}
+        ALT{" "}
+      </div>
       {/* <Image className="row-span-1 cols-span-1" src="/plus.svg" alt="plus" width={220} height={160} /> */}
-      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-3 md:col-span-2 sm:col-span-1 text-center  "> NEU </div>
-      
+      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-3 md:col-span-2 sm:col-span-1 text-center  ">
+        {" "}
+        NEU{" "}
+      </div>
+      <Contact />
     </div>
   );
 }
---
+++ components/ContactUsBanner/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="row-span-1 col-span-4 text-5xl flex justify-between">
+      <p>PROJECT COMING UP?</p>
+      <p>CONTACT US</p>
+    </div>
+  );
+};
+
+export default Contact;
