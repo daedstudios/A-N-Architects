93c7b1f6081a59375a767c8ea5241a52d9a48125
eliasheininger <eliasheininger@web.de>
2025-03-07 17:46:31 +0700

    set up grid structure while akshat was asleep

--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-left justify-top min-h-screen text-4xl">
-      <Image src="/Logo.svg" alt="Vercel Logo" width={920} height={160} />
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-auto grid-rows-[repeat(11,_22.5rem)]  px-2 gap-2 min-h-screen text-4xl">
+      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-4 md:col-span-2 sm:col-span-1 place-content-end"> ALT </div>
+      {/* <Image className="row-span-1 cols-span-1" src="/plus.svg" alt="plus" width={220} height={160} /> */}
+      <div className="row-span-1 text-[8rem] md:text-[12rem] lg:text-[17rem] lg:col-span-3 md:col-span-2 sm:col-span-1 text-center  "> NEU </div>
+      
     </div>
   );
 }
--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -3,16 +3,24 @@ import React from "react";
 
 const TopNav = () => {
   return (
-    <div className="h-[5.5rem] bg-background flex justify-between items-center px-4">
+    <div className="h-[5.5rem] bg-backgorund flex justify-between items-center px-4">
       <div className=" text-1xl">
         <p>A+N</p>
       </div>
 
       <div className="flex gap-4  justify-end">
-        <Link href="/">home</Link>
-        <Link href="/projects">projects</Link>
-        <Link href="/contact">contact</Link>
-        <Link href="/team">team</Link>
+        <Link className="underline-hover" href="/">
+          home
+        </Link>
+        <Link className="underline-hover" href="/projects">
+          projects
+        </Link>
+        <Link className="underline-hover" href="/contact">
+          contact
+        </Link>
+        <Link className="underline-hover" href="/team">
+          team
+        </Link>
       </div>
     </div>
   );
