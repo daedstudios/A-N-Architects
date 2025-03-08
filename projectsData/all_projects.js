4e85a5dfb7f72f4ec19efc592a7d15c6abe8d9d4
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-03-08 17:37:02 +0700

    adding proijects data

---
+++ projectsData/all_projects.js
@@ -0,0 +1,30 @@
+export const projects_data = [
+  {
+    name: "Project 1",
+    description: "This is project 1",
+    details: "latest project description",
+    image: "project1.png",
+    slug: "project1",
+  },
+  {
+    name: "Project 2",
+    description: "This is project 2",
+    details: "latest project description",
+    image: "project2.png",
+    slug: "project2",
+  },
+  {
+    name: "Project 3",
+    description: "This is project 3",
+    details: "latest project description",
+    image: "project3.png",
+    slug: "project3",
+  },
+  {
+    name: "Project 4",
+    description: "This is project 4",
+    details: "latest project description",
+    image: "project4.png",
+    slug: "project4",
+  },
+];
