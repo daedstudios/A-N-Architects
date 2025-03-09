afe1043ad7a340232f79574f1bf80abd9e45a83b
Elias Heininger <eliasheininger@web.de>
2025-03-09 16:08:31 +0700

    added team page and teamData (#19)

---
+++ projectsData/teamData.js
@@ -0,0 +1,26 @@
+export const team_data = [
+
+  {
+    name: "Lea Schneider",
+    details: "Innenarchitektin",
+    image: "team1.png",
+  },
+  {
+    name: "David Meier",
+    details: "Architekt",
+    image: "team2.png",
+    slug: "architekt",
+  },
+  {
+    name: "Sebastian Hoffmann",
+    details: "Bauleiter",
+    image: "team3.png",
+  },
+  {
+    name: "Lukas Weber",
+    details: "Denkmalpfleger",
+    image: "team4.png",
+  },
+];
+
+
