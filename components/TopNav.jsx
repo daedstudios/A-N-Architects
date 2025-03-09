9fd10aa5e6bc7dbefdb731fbca7ad86f279f9002
Akshat Mittal <93286254+drifter089@users.noreply.github.com>
2025-03-09 16:10:29 +0700

    nav code (#21)

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -1,15 +1,65 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 const TopNav = () => {
-  return (
-    <div className="h-[5.5rem] bg-backgorund border-b-[0.5px] top-0 z-100 bg-background sticky flex justify-between items-center px-4">
-      <div className=" text-1xl">
-        <p>A+N</p>
-      </div>
+  const ref = React.useRef(null);
 
-      <div className="gap-4 hidden md:flex justify-end">
+  const [open, setOpen] = React.useState(false);
+
+  useEffect(() => {
+    if (open) {
+      gsap.to(ref.current, { height: "100vh", duration: 0.4 });
+    } else {
+      gsap.to(ref.current, { height: "0", duration: 0.4 });
+    }
+  }, [open]);
+
+  return (
+    <>
+      <div
+        className={`h-[5.5rem] border-b-[0.5px] top-0 z-100 bg-background sticky flex justify-between items-center px-4`}
+      >
+        <div className=" text-1xl">
+          <p>A+N</p>
+        </div>
+
+        <div className="gap-4 hidden md:flex justify-end">
+          <Link className="underline-hover" href="/">
+            home
+          </Link>
+          <Link className="underline-hover" href="/projects">
+            projects
+          </Link>
+          <Link className="underline-hover" href="/contact">
+            contact
+          </Link>
+          <Link className="underline-hover" href="/team">
+            team
+          </Link>
+        </div>
+        <div
+          className={`block gap-4 md:hidden justify-end`}
+          onClick={() => setOpen(!open)}
+        >
+          <Image
+            src="/plus.svg"
+            width={24}
+            height={24}
+            className="tetx-white fill-white"
+          ></Image>
+        </div>
+      </div>
+      <div
+        className="flex text-xl flex-col items-end md:hidden w-screen h-0 bg-background text-foreground px-8 overflow-hidden gap-2 pt-8"
+        ref={ref}
+        onClick={() => setOpen(!open)}
+      >
         <Link className="underline-hover" href="/">
           home
         </Link>
@@ -23,15 +73,7 @@ const TopNav = () => {
           team
         </Link>
       </div>
-      <div className="gap-4 md:hidden justify-end">
-        <Image
-          src="/plus.svg"
-          width={24}
-          height={24}
-          className="text-black"
-        ></Image>
-      </div>
-    </div>
+    </>
   );
 };
 
