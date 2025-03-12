836fd4068f9f4629488e3b0c6e5b24608d7bf4c1
rb0298 <bansalrahul194@gmail.com>
2025-03-12 18:03:09 +0530

    link to contact page added

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ImageComponent from "@/components/ImageCard";
 import { team_data } from "@/projectsData/teamData";
+import ProjectAndContact from "@/components/ProjectAndContact";
 
 const page = () => {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-[auto] md:grid-rows-[repeat(5,_18rem)] grid-rows-[repeat(9,_18rem)] lg:grid-rows-[repeat(3,_18rem)] px-2 gap-2 min-h-screen text-4xl">
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-[auto] md:grid-rows-[repeat(6,_18rem)] grid-rows-[repeat(11,_18rem)] lg:grid-rows-[repeat(4,_18rem)] px-2 gap-2 min-h-screen text-4xl">
       <div className="col-span-1 text-gray-200 lg:col-span-4 md:col-span-2 row-span-1 text-[1.75rem] mt-auto">
         UNSER TEAM
       </div>
@@ -13,6 +14,7 @@ const page = () => {
           <ImageComponent image={image} details={details} name={name} />
         </React.Fragment>
       ))}
+      <ProjectAndContact />
     </div>
   );
 };
