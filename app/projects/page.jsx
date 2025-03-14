99516c1692df0cbdf30e65c1fea14852106f0be8
Elias Heininger <eliasheininger@web.de>
2025-03-14 19:48:03 +0700

    made changes to footer, spacing, added contact section in home, fixed layout issue on md (#38)

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -3,11 +3,9 @@ import { projects_data } from "@/projectsData/all_projects";
 import React from "react";
 import ProjectAndContact from "@/components/ProjectAndContact";
 
-
-
 const page = () => {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-[auto] grid-rows-[repeat(11,_18rem)] md:grid-rows-[repeat(6,_18rem)] lg:grid-rows-[repeat(4,_18rem)] px-2 gap-2 min-h-screen text-4xl">
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-[auto] grid-rows-[repeat(10,_18rem)] md:grid-rows-[repeat(6,_18rem)] lg:grid-rows-[repeat(4,_18rem)] px-2 gap-2 min-h-screen text-4xl">
       <div className="col-span-1 text-gray-200 lg:col-span-4 md:col-span-2 row-span-1 text-[1.75rem] mt-auto">
         PROJEKTE
       </div>
